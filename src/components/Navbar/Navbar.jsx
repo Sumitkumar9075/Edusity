165b486dd71f92f71436c56e04735ec2707f57ce
Sumitkumar9075 <sumitkumar18886@gmail.com>
2024-06-27 10:55:40 +0530

    Completed Header section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,15 +4,17 @@ import logo from '../../assets/edusity_assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='container'>
 
-        <img src={logo} alt="" />
+        <img src={logo} alt="" className='logo' />
         <ul>
           <li>Home</li>
           <li>Program</li>
           <li>About us</li>
           <li>Campus</li>
           <li>Testimonials</li>
+          <li> <button className='btn'>Contact Us</button></li>
+
 
 
 
