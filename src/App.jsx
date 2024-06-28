99122c54b7e75033bebb79866a5d9955706c5d76
Sumitkumar9075 <sumitkumar18886@gmail.com>
2024-06-28 12:46:53 +0530

    Completd video section and made every componennt responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
-import About from './components/About/About'
+import About  from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false)
+
+
+
+
+
   return (
     <div>
 
@@ -18,7 +26,7 @@ const App = () => {
       <div className="container">
         <Title subTitle = 'Our Program' title = 'What we offer'/>
       <Programs/>
-      <About/>
+      <About  setPlayState={ setPlayState}/>
       <Title subTitle = 'Gallery' title = 'Campus Photos'/>
       <Campus/>
       <Title subTitle = 'Testimonials' title = 'What student says'/>
@@ -28,6 +36,8 @@ const App = () => {
       <Footer/>
 
       </div>
+      <VideoPlayer playState ={playState} setPlayState = {setPlayState}/>
+
       
     </div>
   )
